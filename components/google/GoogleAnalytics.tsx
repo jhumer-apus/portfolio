d815171b1bbb13879814741cde4e9ef4eae83312
Jhumer Apus <jhumerapus@gmail.com>
2026-09-18 03:25:53 +0800

    update env variable

--- components/google/GoogleAnalytics.tsx
+++ components/google/GoogleAnalytics.tsx
@@ -2,7 +2,8 @@
 import Script from "next/script";
 
 export default function GoogleAnalytics() {
-  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+  const GA_ID = process.env.GA_ID;
+  console.log("GA_ID:", GA_ID);
 
   if (!GA_ID) return null;
 
